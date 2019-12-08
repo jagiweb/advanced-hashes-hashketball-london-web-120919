954d009608e2573762132c5968aea6c6ff1c9739
Javier Goncalves <jagi.ccs@gmail.com>
2019-12-08 13:58:41 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -117,12 +117,12 @@ def game_hash
   }
 end
 
-def num_points_scored(name)
+def num_points_scored(player_name)
   game_hash.each do |place, team|
     team.each do |attribute, data|
-      if attribute == :players
+      if attribute == players:
         data.each do |player|
-          if player[:player_name] == name
+          if player[player_name:] == players_name
             return player[:points]
           end
         end
