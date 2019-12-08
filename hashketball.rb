ee9ac3f53204d443061f5acb4aa3766982e56ce8
Javier Goncalves <jagi.ccs@gmail.com>
2019-12-08 14:26:34 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -2,62 +2,6 @@
 require 'pry'
 def game_hash
   hash = {
-    home: {
-      team_name: "Brooklyn Nets",
-      colors: ["Black", "White"],
-      players: [
-        {
-          player_name: "Alan Anderson",
-          number: 0,
-          shoe: 16,
-          points: 22,
-          rebourds: 12,
-          assists: 12,
-          steals: 3,
-          blocks: 1,
-          slam_dunks: 1},
-          {
-            player_name: "Reggie Evans",
-            number: 30,
-            shoe: 14,
-            points: 12,
-            rebourds: 12,
-            assists: 12,
-            steals: 12,
-            blocks: 12,
-            slam_dunks: 7},
-            {
-              player_name: "Brook Lopez",
-              number: 11,
-              shoe: 17,
-              points: 17,
-              rebourds: 19,
-              assists: 10,
-              steals: 3,
-              blocks: 1,
-              slam_dunks: 15},
-              {
-                player_name: "Manson Plumlee",
-                number: 1,
-                shoe: 19,
-                points: 26,
-                rebourds: 11,
-                assists: 6,
-                steals: 3,
-                blocks: 8,
-                slam_dunks: 5},
-                {
-                  player_name: "Jason Terry",
-                  number: 31,
-                  shoe: 15,
-                  points: 19,
-                  rebourds: 2,
-                  assists: 2,
-                  steals: 4,
-                  blocks: 11,
-                  slam_dunks: 1}
-          ]
-        },
     away: {
       team_name: "Charlotte Hornets",
       colors: ["Turquoise" , "Purple"],
@@ -113,7 +57,62 @@ def game_hash
                   blocks: 5,
                   slam_dunks: 12}
           ]
-        }
+        },
+        home: {team_name: "Brooklyn Nets",
+               colors: ["Black", "White"],
+               players: [
+                  { player_name: "Alan Anderson",
+                    number: 0,
+                    shoe: 16,
+                    points: 22,
+                    rebourds: 12,
+                    assists: 12,
+                    steals: 3,
+                    blocks: 1,
+                    slam_dunks: 1
+                  },
+                  { player_name: "Reggie Evans",
+                    number: 30,
+                    shoe: 14,
+                    points: 12,
+                    rebourds: 12,
+                    assists: 12,
+                    steals: 12,
+                    blocks: 12,
+                    slam_dunks: 7},
+                  {
+                    player_name: "Brook Lopez",
+                    number: 11,
+                    shoe: 17,
+                    points: 17,
+                    rebourds: 19,
+                    assists: 10,
+                    steals: 3,
+                    blocks: 1,
+                    slam_dunks: 15
+                  },
+                  { player_name: "Manson Plumlee",
+                    number: 1,
+                    shoe: 19,
+                    points: 26,
+                    rebourds: 11,
+                    assists: 6,
+                    steals: 3,
+                    blocks: 8,
+                    slam_dunks: 5
+                  },
+                  { player_name: "Jason Terry",
+                    number: 31,
+                    shoe: 15,
+                    points: 19,
+                    rebourds: 2,
+                    assists: 2,
+                    steals: 4,
+                    blocks: 11,
+                    slam_dunks: 1
+                  }
+              ]
+            }
   }
 end
 
