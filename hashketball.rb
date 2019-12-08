228c3a2683b7e1d1218d8e2d9e0889810b27343a
Javier Goncalves <jagi.ccs@gmail.com>
2019-12-08 15:08:13 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -263,12 +263,13 @@ def team_names
 end
 
 def player_numbers(team_name)
-  game_hash.map do |place, team|
+  numbers = []
+  game_hash.each do |place, team|
     if team[:team_name] == team_name
       team.each do |attribute, data|
         if attribute == :players
           data.each do |player|
-            return player[:number]
+            numbers player[:number]
           end
         end
       end
