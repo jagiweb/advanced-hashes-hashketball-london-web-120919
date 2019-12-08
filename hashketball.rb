bf641189aeebde50aef7b04b142f7eb97afe06fc
Javier Goncalves <jagi.ccs@gmail.com>
2019-12-08 11:19:35 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,46 +1,37 @@
 # Write your code here!
-def game_hash 
+def game_hash
   hash = {
     home: {
       team_name: "Brooklyn Nets",
-      colors: ["Black", "White"], 
+      colors: ["Black", "White"],
       players: [
         {
-          player_name: "", 
-          number: , 
-          shoe: , 
-          points: , 
-          rebourds: , 
-          assists: , 
-          steals: , 
-          blocks: , 
+          player_name: "",
+          number: ,
+          shoe: ,
+          points: ,
+          rebourds: ,
+          assists: ,
+          steals: ,
+          blocks: ,
           slam_dunks: },
           ]
         }
     away: {
-      team_name: "", 
-      colors: [""], 
+      team_name: "",
+      colors: [""],
       players: [
         {
-          player_name: "", 
-          number: , 
-          shoe: , 
-          points: , 
-          rebourds: , 
-          assists: , 
-          steals: , 
-          blocks: , 
+          player_name: "",
+          number: ,
+          shoe: ,
+          points: ,
+          rebourds: ,
+          assists: ,
+          steals: ,
+          blocks: ,
           slam_dunks: },
           ]
         }
   }
 end
-
-
-
-
-
-
-
-
-
