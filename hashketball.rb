ff8c2bc960bcb4477806e595b5e81539c995448a
Javier Goncalves <jagi.ccs@gmail.com>
2019-12-08 14:31:12 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -220,91 +220,13 @@ def game_hash
   }
 end
 
-# def num_points_scored(players_name)
-#   game_hash.each do |place, team|
-#     team.each do |attribute, data|
-#       if attribute == :players
-#         data.each do |player|
-#           if player[:player_name] == players_name
-#             return player[:points]
-#           end
-#         end
-#       end
-#     end
-#   end
-# end
-#
-# def shoe_size
-#   game_hash.each do |place, team|
-#     team.each do |attribute, data|
-#       if attribute == :players
-#         data.each do |player|
-#           if player[:player_name] == players_name
-#             return player[:shoe]
-#           end
-#         end
-#       end
-#     end
-#   end
-# end
-
-def num_points_scored(player_search)
-  game_hash.each do |team, team_info|
-    team_info[:players].each do |player|
-      if player[:player_name] == player_search
-        return player[:points]
-      end
-    end
-  end
-end
-
-def shoe_size(name)
-  game_hash.each do |team, team_info|
-    team_info[:players].each do |player|
-      if player[:player_name] == name
-        return player[:shoe]
-      end
-    end
-  end
-end
-
-def team_colors(team_input)
-  if team_input.downcase == "charlotte hornets"
-    return game_hash[:away][:colors]
-  else return game_hash[:home][:colors]
-  end
-end
-
-def team_names
-  game_hash.map do |team, team_info|
-    team_info[:team_name]
-  end
-end
-
-def player_numbers(input)
-  output = []
-  game_hash.each do |team, team_info|
-    if team_info[:team_name] == input
-      team_info.each do |key, value|
-        if key == :players
-          value.each do |player|
-          output.push(player[:number])
-          end
-        end
-      end
-    end
-  end
-  return output
-end
-
-def player_stats(input)
-  game_hash.each do |team, team_info|
-    team_info.each do |key, value|
-      if key == :players
-        value.each do |player|
-          if input == player[:player_name]
-            player.delete(:player_name) # having player name inside the hash was a bad idea!
-            return player
+def num_points_scored(players_name)
+  game_hash.each do |place, team|
+    team.each do |attribute, data|
+      if attribute == :players
+        data.each do |player|
+          if player[:player_name] == players_name
+            return player[:points]
           end
         end
       end
@@ -312,16 +234,94 @@ def player_stats(input)
   end
 end
 
-def big_shoe_rebounds
-  big_shoe = 0
-  rebounds = 0
-  game_hash.each do |team, team_info|
-    team_info[:players].each do |player|
-      if player[:shoe] > big_shoe
-        big_shoe = player[:shoe]
-        rebounds = player[:rebounds]
+def shoe_size
+  game_hash.each do |place, team|
+    team.each do |attribute, data|
+      if attribute == :players
+        data.each do |player|
+          if player[:player_name] == players_name
+            return player[:shoe]
+          end
+        end
       end
     end
   end
-  return rebounds
 end
+
+# def num_points_scored(player_search)
+#   game_hash.each do |team, team_info|
+#     team_info[:players].each do |player|
+#       if player[:player_name] == player_search
+#         return player[:points]
+#       end
+#     end
+#   end
+# end
+# 
+# def shoe_size(name)
+#   game_hash.each do |team, team_info|
+#     team_info[:players].each do |player|
+#       if player[:player_name] == name
+#         return player[:shoe]
+#       end
+#     end
+#   end
+# end
+# 
+# def team_colors(team_input)
+#   if team_input.downcase == "charlotte hornets"
+#     return game_hash[:away][:colors]
+#   else return game_hash[:home][:colors]
+#   end
+# end
+# 
+# def team_names
+#   game_hash.map do |team, team_info|
+#     team_info[:team_name]
+#   end
+# end
+# 
+# def player_numbers(input)
+#   output = []
+#   game_hash.each do |team, team_info|
+#     if team_info[:team_name] == input
+#       team_info.each do |key, value|
+#         if key == :players
+#           value.each do |player|
+#           output.push(player[:number])
+#           end
+#         end
+#       end
+#     end
+#   end
+#   return output
+# end
+# 
+# def player_stats(input)
+#   game_hash.each do |team, team_info|
+#     team_info.each do |key, value|
+#       if key == :players
+#         value.each do |player|
+#           if input == player[:player_name]
+#             player.delete(:player_name) # having player name inside the hash was a bad idea!
+#             return player
+#           end
+#         end
+#       end
+#     end
+#   end
+# end
+# 
+# def big_shoe_rebounds
+#   big_shoe = 0
+#   rebounds = 0
+#   game_hash.each do |team, team_info|
+#     team_info[:players].each do |player|
+#       if player[:shoe] > big_shoe
+#         big_shoe = player[:shoe]
+#         rebounds = player[:rebounds]
+#       end
+#     end
+#   end
+#   return rebounds
+# end
