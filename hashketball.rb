5d803e0892f2f8794f11fc536ffc0d6deba0e7ca
Javier Goncalves <jagi.ccs@gmail.com>
2019-12-08 15:11:42 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -284,7 +284,7 @@ def player_stats(players_name)
       if attribute == :players
         data.each do |player|
           if player[:player_name] == players_name
-            player
+            
           end
         end
       end
