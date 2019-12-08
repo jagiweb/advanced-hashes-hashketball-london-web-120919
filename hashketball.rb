7e23b5aadfd5d4a3a3d58046baed444e511c7380
Javier Goncalves <jagi.ccs@gmail.com>
2019-12-08 14:14:16 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -125,7 +125,7 @@ def num_points_scored(players_name)
           if player[:player_name] == players_name
             binding.pry
             return player[:points]
-            
+
           end
         end
       end
