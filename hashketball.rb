5d34f73dafa8230408a57da7b58f65fc823ff457
Javier Goncalves <jagi.ccs@gmail.com>
2019-12-08 14:47:12 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -255,3 +255,10 @@ def team_colors(team_name)
     end
   end
 end
+
+def team_names(team_name)
+  game_hash.each do |place, team|
+    if team[:team_name] == team_name
+      return team[:team_name]
+  end 
+end
